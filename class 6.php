e1dcfb8cf4c51c42d95ff0064635e474abd2740f
nmsaikat <nmsaikat.cf@hmail.com>
2025-07-27 21:10:32 +0600

    class 6 some code update

--- class 6.php
+++ class 6.php
@@ -12,3 +12,9 @@ print_r($student);
 echo $student['fname']. " ". $student['lname']. "\n";
 
 printf("%s %s \n", $student['fname'], $student['lname']);
+
+// echo join(",",$student);
+
+$serialized = serialize($student);
+
+$newstudent = unserialize($serialized);
