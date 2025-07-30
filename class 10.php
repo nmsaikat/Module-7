5dc17a4b3d4b451ac5e13f709eeddf064a53ffef
nmsaikat <nmsaikat.cf@hmail.com>
2025-07-30 10:59:41 +0600

    Class 10 Some Code Addead

--- class 10.php
+++ class 10.php
@@ -4,3 +4,13 @@ $fruits = array ('apple', 'banana', 'orange', 'plum', 'dates', 'mango');
 
 $someFruits = array_slice($fruits, 2);
 print_r($someFruits);
+
+echo "\n";
+echo "\n";
+
+$emp = [
+      [1,"Krishana","Manager",50000],
+      [2,"Salman","Salesman",20000],
+      [3,"Mohan","Computer Operator",12000],
+      [4,"Amir","Driver",5000]
+];
