d4c6c37082e71078938d49a7ba78cf0586151a7a
nmsaikat <nmsaikat.cf@hmail.com>
2025-08-06 19:16:05 +0600

    class 15 done

---
+++ class 15.php
@@ -0,0 +1,13 @@
+<?php
+
+$numbers1 = [1,4,3,66,54,7,23,1,2];
+$numbers2 = [88,3,21,44,3,28,1,2,7,];
+
+$fruits1 = ["a"=>"apple", "b"=>"banana", "c"=>"lemon"];
+$fruits2 = ["d"=>"pineapple", "b"=>"malta", "d"=>"grapes", "e"=>"lemon"];
+
+$common = array_intersect($numbers1, $numbers2);
+$commonf = array_intersect($fruits1, $fruits2);
+print_r($common);
+echo "\n";
+print_r($commonf);
